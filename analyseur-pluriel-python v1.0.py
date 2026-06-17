34eb99001a64cd2bcf46a7fb6b0b0b99bfc6466a
ay-TAL <aymenkhedimallah0@gmail.com>
2026-06-17 03:14:38 +0100

    Add files via upload

---
+++ analyseur-pluriel-python v1.0.py
@@ -0,0 +1,16 @@
+exceptions = {"souris": "singulier", "bus": "singulier", "choix": "singulier", "prix": "singulier"}
+
+ter =('s','x')
+
+def mots(word):
+ if word in exceptions:
+   return('ce mot est au singulier')
+ else:
+    if word.endswith(ter):
+     return('ce mot est au pluriel')
+    else:
+     return('ce mot est au singulier')
+  
+res = mots(input('quelle est le mot: '))
+
+print(res)
