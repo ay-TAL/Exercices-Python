d2f44fac0dbce5f2ce92532282db2c3e8efaec86
ay-TAL <aymenkhedimallah0@gmail.com>
2026-06-18 04:16:53 +0100

    Refactor word filtering and input handling

--- mot court ou long.py
+++ mot court ou long.py
@@ -1,19 +1,21 @@
-def filtrer_mot(mot,limit):
-
-
-     if len(mot) < limit:
-        return('est mot trop court ')
-     else:
-        return('mot valide')
+def filtrer_mot(mot, limit):
+    if len(mot) < limit:
+        return 'le mot est trop court'
+    else:
+        return 'mot valide'
 
 def analyse_saisie():
-    ver = input('veuiller saisire un mot: ')   
-    return(ver)
+    ver = input('veuiller saisire un mot: ')
+    return ver
 
 def combien():
     cmb = input('veuiller attribuer le nombre: ')
-    return(cmb)
+    return cmb
 
-res = filtrer_mot(analyse_saisie(),int(combien()))
+mot_utilisateur = analyse_saisie()
+limite_texte = combien()
+limite_chiffre = int(limite_texte)
+
+res = filtrer_mot(mot_utilisateur, limite_chiffre)
 
 print(res)
